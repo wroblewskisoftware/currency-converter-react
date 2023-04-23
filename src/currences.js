08cdf7841acd5603024ef727d97307c153f9da03
Radoslaw Wroblewski <wroblewski082@gmail.com>
2023-04-23 19:35:24 +0200

    added array of objects named currencies

---
+++ src/currences.js
@@ -0,0 +1,24 @@
+const currences = [
+  {
+    name: "Euro",
+    shortName: "eur",
+    value: "4,6039",
+  },
+  {
+    name: "Dolar amerykański",
+    shortName: "usd",
+    value: "4,2006",
+  },
+  {
+    name: "Funt szterling",
+    shortName: "gbp",
+    value: "5,2086",
+  },
+  {
+    name: "Frank szwajcarski",
+    shortName: "chf",
+    value: "4,7010",
+  },
+];
+
+export default currences;
