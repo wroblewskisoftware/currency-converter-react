badcbf605a6969b397d59119b85d52ee33b69a2d
Radoslaw Wroblewski <wroblewski082@gmail.com>
2023-05-01 19:32:29 +0200

    fixed the writing of numbers in the value properties of the currencies object

--- src/currences.js
+++ src/currences.js
@@ -2,22 +2,22 @@ const currences = [
   {
     name: "Euro",
     shortName: "eur",
-    value: "4,6039",
+    value: "4.6039",
   },
   {
     name: "Dolar amerykański",
     shortName: "usd",
-    value: "4,2006",
+    value: "4.2006",
   },
   {
     name: "Funt szterling",
     shortName: "gbp",
-    value: "5,2086",
+    value: "5.2086",
   },
   {
     name: "Frank szwajcarski",
     shortName: "chf",
-    value: "4,7010",
+    value: "4.7010",
   },
 ];
 
