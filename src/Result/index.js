6d8c32d292ea4e126b2ed03f00184992cd438171
Radoslaw Wroblewski <wroblewski082@gmail.com>
2023-05-15 19:49:57 +0200

    added "&nbsp;" to the "Result" component

--- src/Result/index.js
+++ src/Result/index.js
@@ -2,9 +2,10 @@ import "./style.css";
 
 const Result = ({ result }) => (
   <p className="result">
-    W dniu 23.04.2023r. za <strong>{result.amount} PLN</strong> możesz nabyć{" "}
+    W dniu 23.04.2023r. za <strong>{result.amount}&nbsp;PLN</strong> możesz
+    nabyć{" "}
     <strong>
-      {result.result.toFixed(2)} {result.currency}
+      {result.result.toFixed(2)}&nbsp;{result.currency}
     </strong>
     .
   </p>
