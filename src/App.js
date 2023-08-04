8d784ea0d1fe7bf3380482eda09e170bf7054413
Radoslaw Wroblewski <wroblewski082@gmail.com>
2023-08-04 19:47:17 +0200

    moved GlobalStyle to index.js file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import GlobalStyle from "./globalStyles";
 import { Container } from "./Container/styled";
 import Form from "./Form";
 import { Fieldset } from "./Fieldset/styled";
@@ -48,41 +47,38 @@ function App() {
   };
 
   return (
-    <>
-      <GlobalStyle />
-      <Container>
-        <Form calculateResult={calculateResult} resetAll={resetAll}>
-          <Fieldset>
-            <Legend name="Kalkulator walut" />
-            <DateField />
-            <Information content="Pola wymagane są oznaczone*." />
-            <Section
-              label="Podaj kwotę w PLN*:"
-              field={<Input amount={amount} onInputChange={onInputChange} />}
-            />
-            <Section
-              label="Wybierz walutę:"
-              field={
-                <Options
-                  currencies={currencies}
-                  selectedCurrency={selectedCurrency}
-                  onCurrencyChange={onCurrencyChange}
-                />
-              }
-            />
-          </Fieldset>
-          <Buttons
-            content={
-              <>
-                <Button name="Przelicz" />
-                <Button name="Wyczyść" type="reset" />
-              </>
+    <Container>
+      <Form calculateResult={calculateResult} resetAll={resetAll}>
+        <Fieldset>
+          <Legend name="Kalkulator walut" />
+          <DateField />
+          <Information content="Pola wymagane są oznaczone*." />
+          <Section
+            label="Podaj kwotę w PLN*:"
+            field={<Input amount={amount} onInputChange={onInputChange} />}
+          />
+          <Section
+            label="Wybierz walutę:"
+            field={
+              <Options
+                currencies={currencies}
+                selectedCurrency={selectedCurrency}
+                onCurrencyChange={onCurrencyChange}
+              />
             }
           />
-        </Form>
-        {result ? <Result result={result} /> : null}
-      </Container>
-    </>
+        </Fieldset>
+        <Buttons
+          content={
+            <>
+              <Button name="Przelicz" />
+              <Button name="Wyczyść" type="reset" />
+            </>
+          }
+        />
+      </Form>
+      {result ? <Result result={result} /> : null}
+    </Container>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import GlobalStyle from "./globalStyles";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <GlobalStyle />
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
