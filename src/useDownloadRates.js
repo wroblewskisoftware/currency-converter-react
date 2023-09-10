7fd49d4f6c2c88e97876282380c38cb65a05d3fc
Radoslaw Wroblewski <wroblewski082@gmail.com>
2023-09-10 20:45:39 +0200

    axios package installed

--- src/useDownloadRates.js
+++ src/useDownloadRates.js
@@ -1,10 +1,11 @@
 import { useEffect } from "react";
+import axios from "axios";
 
 export const useDownloadRates = () => {
   useEffect(() => {
     (async () => {
       try {
-        const response = await axios.get("./data.json");
+        const response = await axios.get("../data.json");
         console.log(response.data);
       } catch (error) {
         console.error("Coś poszło nie tak, sprawdź połączenie z internetem.");
