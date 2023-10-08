d4d090c8079c385966a4901e2ed4d2565d525b06
Radoslaw Wroblewski <wroblewski082@gmail.com>
2023-10-08 20:38:40 +0200

    useDownloadRates hook call removed

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,8 +1,4 @@
-import { useDownloadRates } from "../useDownloadRates";
-
 const Form = ({ children, calculateResult, resetAll }) => {
-  useDownloadRates();
-
   const onFormSubmit = (event) => {
     event.preventDefault();
     calculateResult();
