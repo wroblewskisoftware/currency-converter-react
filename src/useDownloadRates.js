b8ea10bdb532367a4ef54608dd400f3228bcd23f
Radoslaw Wroblewski <wroblewski082@gmail.com>
2023-10-08 20:36:56 +0200

    ratesData state variable added

--- src/useDownloadRates.js
+++ src/useDownloadRates.js
@@ -1,17 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 export const useDownloadRates = () => {
+  const [ratesData, setRatesData] = useState();
+
   useEffect(() => {
     (async () => {
       try {
         const response = await axios.get("data.json");
-        const dataObject = response.data;
-        console.log(dataObject);
+        const saveData = () => setRatesData(response.data);
+        saveData();
       } catch (error) {
-        const isError = true;
-        console.log(isError);
+        const saveError = () => setRatesData({ error: true });
+        saveError();
       }
     })();
   }, []);
+
+  return ratesData;
 };
