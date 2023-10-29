692355bc28a31ffaa93b12ec809ac049deb8dd92
Radoslaw Wroblewski <wroblewski082@gmail.com>
2023-10-29 13:26:22 +0100

    changed api in custom hook useDownloadRates.js

--- src/useDownloadRates.js
+++ src/useDownloadRates.js
@@ -8,8 +8,10 @@ export const useDownloadRates = () => {
     setTimeout(() => {
       (async () => {
         try {
-          const response = await axios.get("data.json");
-          setRatesData(response.data);
+          const response = await axios.get(
+            "https://api.currencyapi.com/v3/latest?apikey=cur_live_4qw0WYNKjAF1Gd66sEvBhzcKSBKclgInv66XokCL&currencies=EUR%2CUSD%2CGBP%2CCHF&base_currency=PLN"
+          );
+          setRatesData({ ...response.data, success: true });
         } catch (error) {
           setRatesData({ error: true });
         }
