7caac8987d9a4561678c58964d8ccaa5ade9321b
Radoslaw Wroblewski <wroblewski082@gmail.com>
2023-10-29 13:31:28 +0100

    changed currenciesArray variable  in Options component

--- src/Options/index.js
+++ src/Options/index.js
@@ -1,7 +1,7 @@
 import { StyledInput } from "../Input/styled";
 
 const Options = ({ ratesData, selectedCurrency, onCurrencyChange }) => {
-  const currenciesArray = Object.keys(ratesData.rates);
+  const currenciesArray = Object.keys(ratesData.data);
 
   return (
     <StyledInput
