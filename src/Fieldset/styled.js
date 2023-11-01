f137918e41d98dde27209ff8002d4bb921effb58
Radoslaw Wroblewski <wroblewski082@gmail.com>
2023-11-01 19:06:14 +0100

    changed styling of the Fieldset component

--- src/Fieldset/styled.js
+++ src/Fieldset/styled.js
@@ -1,9 +1,14 @@
 import styled from "styled-components";
 
 export const Fieldset = styled.fieldset`
+  min-height: 284px;
   border: 2px solid ${({ theme }) => theme.color.black};
   border-radius: 5px;
   padding: 20px;
   margin: 0 5px;
   background-color: ${({ theme }) => theme.color.sand};
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    min-height: 382px;
+  }
 `;
